18ef97d569387724044a8da8d07b35580e1bf582
paulovictor11 <pauloalmeidajp652@gmail.com>
2021-09-09 15:45:16 -0300

    Recurso de bd do firebase; Criação de sala; Formulários; Tela de perguntas da sala; Recurso para criação de nova pergunta; Recurso para ficar observando as perguntas no bd;

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,13 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -21,12 +24,29 @@ export function Home() {
         history.push('/rooms/new');
     }
 
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+        
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`);
+    }
+
     return (
         <div id="page-auth">
             <aside>
                 <img src={ illustrationImg } alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+                <strong>Toda pergunta tem uma resposta.</strong>
+                <p>Aprenda e compartilhe conhecimento com outras pessoas.</p>
             </aside>
             <main>
                 <div className="main-content">
@@ -36,8 +56,13 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Digite o código da sala"/>
+                    <form onSubmit={ handleJoinRoom }>
+                        <input
+                            type="text"
+                            placeholder="Digite o código da sala"
+                            onChange={ event => setRoomCode(event.target.value) }
+                            value={ roomCode }
+                        />
                         <Button type="submit">Entre na sala</Button>
                     </form>
                 </div>
