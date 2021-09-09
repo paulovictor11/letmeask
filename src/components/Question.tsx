e3bcfcb76c9bd6068a41d6e88a5019fcac6fc2e3
paulovictor11 <pauloalmeidajp652@gmail.com>
2021-09-09 20:02:22 -0300

    Criação de hooks; Página de sala do admin; Recurso de like; Recurso para remoção de pergunta; Recurso para encerrar a sala;

---
+++ src/components/Question.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import '../styles/question.scss';
+
+type QuestioProps = {
+    content: string;
+    author: {
+        name: string;
+        avatar: string;
+    };
+    children?: ReactNode
+}
+
+export function Question(props: QuestioProps) {
+    return (
+        <div className="question">
+            <p>{ props.content }</p>
+            <footer>
+                <div className="user-info">
+                    <img src={ props.author.avatar } alt={ props.author.name } />
+                    <span>{ props.author.name }</span>
+                </div>
+                <div>
+                    { props.children }
+                </div>
+            </footer>
+        </div>
+    );
+}
